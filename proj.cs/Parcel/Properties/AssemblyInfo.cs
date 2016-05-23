244cae23d36014f344b87b3f45f22c8b810b882e
Byron Mayne <Byron Mayne>
2016-05-23 08:55:37 -0400

    First commit of the project. Added the MSBuild steps

---
+++ proj.cs/Parcel/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OIGC2016")]
+[assembly: AssemblyDescription("This is a project used to show examples of taking advantage of MSBuild with a Unity project.")]
+[assembly: AssemblyCompany("ByronMayne")]
+[assembly: AssemblyProduct("OIGC2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6c538682-3481-4838-b43b-740a1ce1463a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
